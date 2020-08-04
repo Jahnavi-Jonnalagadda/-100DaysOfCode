c9f80bce216f29e7d479d1a7b007dade9492afe3
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-08-04 21:41:28 +0530

    Day-6

---
+++ Buying_slots.py
@@ -0,0 +1,7 @@
+'''Along one side of a road there is a sequence of vacant plots of land.
+Each plot has a different area(non-zero). So, the areas form a sequence
+A[1],A[2], ..., A[N].
+
+You want to buy K acres of land to build a house. You want to find a segment of
+contigious plots(i.e., a subsection in a sequence) of minimum length whose sum
+is exactly K.'''
