0aeeeec8f723cf95da09c66bd4d2cdba6daa4bb4
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-08-15 21:50:46 +0530

    Day-16

---
+++ Contests/Coding_Club-15th_Aug/Harsh's_Happiness.py
@@ -0,0 +1,14 @@
+def harsh_happy(arr, k):
+    for j in range(len(arr)):
+        if(arr[j]>k):
+            return True
+    return False
+
+t = int(input())
+for i in range(t):
+    n,k = map(int,input().split())
+    arr = list(map(int,input().split()))
+    if(harsh_happy(arr,k) == True):
+        print("Yes")
+    else:
+        print("No")
