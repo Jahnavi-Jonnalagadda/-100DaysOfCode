e6801db89694f17517d880959a3dcd7fe0a00ad1
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-08-16 08:20:28 +0530

    Day-17

---
+++ Contests/Coding_Club-15th_Aug/Member_Count.py
@@ -0,0 +1,16 @@
+def member_count(n):
+    i = 1
+    while(((i*(i-1))//2) <= n):
+        if(((i*(i-1))//2) == n):
+            return True
+        i += 1
+    return False
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if(member_count(n) == True):
+        print("Possible")
+    else:
+        print("Impossible")
+    
