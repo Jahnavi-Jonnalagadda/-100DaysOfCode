815f3ba50ab37061fda4eac53fb56aecdda3df31
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-08-20 00:31:27 +0530

    Day-20

--- LeetCode/Easy/Plus_One.py
+++ LeetCode/Easy/Plus_One.py
@@ -1,14 +1,18 @@
 def plus_one(arr):
-    l = len(digits)
-    for i in range(l-1,-1,-1):
-        if(digits[i]<9):
-            digits[i] += 1
-            return digits
-        digits[i] = 0
-        
-    final_array = [0]*(l+1)
-    final_array[0] = 1
-    return final_array
+    n = len(arr)
+    arr[n-1] += 1
+    carry = arr[n-1]//10
+    arr[n-1] = arr[n-1]%10
 
-digits = list(map(int,input().split()))
-print(plus_one(digits))
+    for i in range(n-2, -1, -1):
+        if(carry == 1):
+            arr[i] += 1
+            carry = arr[i]//10
+            arr[i] = arr[i]%10
+
+    if(carry == 1):
+        arr.insert(0, 1)
+    return arr
+
+arr = list(map(int,input().split()))
+print(plus_one(arr))
