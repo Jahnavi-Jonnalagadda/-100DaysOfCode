9b2a9bc6367c2d2a99f764bcee3282ebd8d38ba4
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-09-03 16:04:07 +0530

    same tree

---
+++ LeetCode/Easy/Same_Tree.py
@@ -0,0 +1,10 @@
+def isSameTree(p, q):
+        if p == None and q == None:
+            return True
+        elif p == None or q == None:
+            return False
+        elif p.val!=q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
