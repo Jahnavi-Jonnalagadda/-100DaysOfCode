d76ef41e6f5e7156702d1b8b2f2fd1b9128fab68
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-09-11 10:42:30 +0530

    Max heap implementation

---
+++ GeeksforGeeks/Arrays/Max_Heap_Implementation.py
@@ -0,0 +1,21 @@
+def maxHeapify(arr, i):
+    leftChild = 2*i + 1
+    rightChild = 2*i + 2
+    size = len(arr)-1
+    largest = i
+    if(leftChild <= size and arr[leftChild] > arr[i]):
+        largest = leftChild
+    if(rightChild <= size and arr[rightChild] > arr[largest]):
+        largest = rightChild
+    if(largest != i):
+        arr[i], arr[largest] = arr[largest], arr[i]
+        maxHeapify(arr, largest)
+
+def buildMaxHeap(arr):
+    for i in range((len(arr)//2)-1, -1, -1):
+        maxHeapify(arr, i)
+    return arr
+
+arr = list(map(int,input().split()))
+print(buildMaxHeap(arr))
+
