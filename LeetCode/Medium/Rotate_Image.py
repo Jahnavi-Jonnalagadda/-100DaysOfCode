48f44b8f3411f9d52c6a8c00394e71b9c590ff31
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-09-14 06:34:43 +0530

    Rotate Image

---
+++ LeetCode/Medium/Rotate_Image.py
@@ -0,0 +1,11 @@
+def rotate(matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        
+        return matrix
