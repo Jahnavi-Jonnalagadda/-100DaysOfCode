799d1750f32d4ed989e666b76dae796fbfe43e23
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-09-16 08:47:02 +0530

    Plus one

--- LeetCode/Easy/Plus_One.py
+++ LeetCode/Easy/Plus_One.py
@@ -1,3 +1,4 @@
+# Approach-1
 def plus_one(arr):
     n = len(arr)
     arr[n-1] += 1
@@ -12,6 +13,34 @@ def plus_one(arr):
 
     if(carry == 1):
         arr.insert(0, 1)
+    return arr
+
+# Approach-2
+def plus_one(arr):
+    n = len(arr)
+    c = 0
+    val = arr[n-1] + 1
+    if(val > 9):
+        arr[n-1] = 0
+        c = 1
+    else:
+        arr[n-1] = val
+    for i in range(len(arr)-2, -1, -1):
+        if(arr[i] + c > 9 and i!=0):
+            arr[i] = 0
+            c = 1
+        elif(arr[i] + c > 9 and i ==0):
+            arr[i] = arr[i]+c
+        else:
+            arr[i] = arr[i]+c
+            c = 0
+    if(arr[0] > 9):
+        return resize(arr)
+    return arr
+
+def resize(arr):
+    arr.insert(0, 1)
+    arr[1] = 0
     return arr
 
 arr = list(map(int,input().split()))
